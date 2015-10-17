e9ab17cd8a1d74a86579464b19cf88aabf2b8ade
Kirk Graves <graves.kirk@gmail.com>
2015-10-16 20:48:52 -0500

    modified the search.js module to work with searches of the string 'the'

--- javascripts/search.js
+++ javascripts/search.js
@@ -9,17 +9,32 @@ define(function(require) {
 
 			var deferred = q.defer();
 			var titleQuery = $('#search').val();
+
+			if (titleQuery === 'The') {
+				$.ajax({
+					type: 'GET',
+					url: 'http://www.omdbapi.com/?s=' + titleQuery
+				}).done(function(data) {
+						deferred.resolve(data);
+						console.log('Data from OMDB API for "The" = ', data);
+					})
+					.fail(function(xhr, status, error) {
+						deferred.reject(error);
+					});
+				
+			} else {
+				$.ajax({
+					type: 'GET',
+					url: 'http://www.omdbapi.com/?t=' + titleQuery
+				}).done(function(data) {
+						deferred.resolve(data);
+						console.log('Data from OMDB API = ', data);
+					})
+					.fail(function(xhr, status, error) {
+						deferred.reject(error);
+					});
+			}
 			
-			$.ajax({
-				type: 'GET',
-				url: 'http://www.omdbapi.com/?t=' + titleQuery
-			}).done(function(data) {
-					deferred.resolve(data);
-					console.log('Data from OMDB API = ', data);
-				})
-				.fail(function(xhr, status, error) {
-					deferred.reject(error);
-				});
 			
 			return deferred.promise;
 
