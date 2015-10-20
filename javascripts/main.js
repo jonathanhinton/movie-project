128b21c1aa936877eb305a5705195432909e3386
Kirk Graves <graves.kirk@gmail.com>
2015-10-19 23:39:33 -0500

    integrated rating system with add-to-firebase functionality

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,16 +6,21 @@ requirejs.config({
     'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
     'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min',
     'q': '../lib/bower_components/q/q',
-    'firebase': '../lib/bower_components/firebase/firebase'
+    'firebase': '../lib/bower_components/firebase/firebase',
+    'bootstrap-star-rating': '../lib/bower_components/bootstrap-star-rating/js/star-rating.min'
   },
     shim: {
-      'bootstrap': ['jquery']
+      'bootstrap': ['jquery'],
+      'bootstrap-star-rating': ['bootstrap']
     }
 });
 
-require(['jquery', 'search', 'getFilms', 'addMovie', 'hbs!../templates/titleSearch'], function($, search, getFilms, addMovie, searchHbs) {
+require(['jquery', 'search', 'getFilms', 'addMovie', 'hbs!../templates/titleSearch','bootstrap-star-rating'], function($, search, getFilms, addMovie, searchHbs, starRating) {
+
+	
 
 	var ref = new Firebase('https://movie-history-project.firebaseio.com');
+	var user = ref.getAuth().uid;
 
   $('#submit').click(function(e) {
     var globalFilmData;
@@ -28,21 +33,49 @@ require(['jquery', 'search', 'getFilms', 'addMovie', 'hbs!../templates/titleSear
         globalFilmData = filmData;
         console.log('globalFilmData', globalFilmData);
         $('#output').html(searchHbs(globalFilmData));
+        $('.stars').rating();
       }).done();
 
+
+    search.searchFirebase(user)
+    	.then(function(firebaseData) {
+    		console.log('firebaseData', firebaseData);
+    	})
   });
 
   $(document).on('click', '.addFilm', function(e) {
 		var filmID = this.id;
-		console.log('filmID', filmID);
+    var value;
+		// console.log('filmID', filmID);
 		getFilms.getFilm(filmID)
 		.then(function(filmObj) {
 	    console.log('filmObj', filmObj);
-	    var user = ref.getAuth().uid;
 	    console.log("user", user);
-	    addMovie.addMovie(user, filmObj);
+	    addMovie.addMovie(user, filmObj, value);
 		});
 
+  });
+
+// Inside this function, we want to insert a conditional statement that says 'if you rate this film, then you watched it,'' and the rating gets pushed (along with the film object) to the user's Firebase
+  
+
+  $(document).on('rating.change', '.stars', function(e, value, caption) {
+    // console.log('value', value);
+    var filmID = this.id;
+    console.log('filmID', filmID);
+    getFilms.getFilm(filmID)
+    .then(function(filmObj) {
+      addMovie.addMovie(user, filmObj, value)  
+    });
+      // console.log('value', value);
+      // console.log('filmObj.imdbID', filmObj.imdbID);
+      // console.log('value', value);
+      // if ((ref.child('profiles').child(user).child(this.id)) === (filmObj.imdbID)) {
+      //   ref.child('profiles').child(user).child(this.id).child('rating').set(value);
+      //   } else {
+      //   }
+  
+    
   });
 
 });
--- javascripts/search.js
+++ javascripts/search.js
@@ -3,15 +3,6 @@ define(function(require) {
 	var $ = require('jquery');
 	var q = require('q');
 
-      var loginRef = new Firebase("https://movie-history-project.firebaseio.com/");
-
-      var ref = new Firebase("movie-history-project.firebaseio.com");
-      var userAuth = ref.getAuth();
-
-      if(userAuth) {
-        console.log("Authenticated user with uid:", userAuth.uid);
-      }
-
 	return {
 
 		searchFilms: function() {
@@ -24,15 +15,35 @@ define(function(require) {
 				type: 'GET',
 				url: 'http://www.omdbapi.com/?s=' + query
 			}).done(function(searchData) {
-                    deferred.resolve(searchData);
-                    console.log('Search data from OMDB API = ', searchData);
+          deferred.resolve(searchData);
+          console.log('Search data from OMDB API = ', searchData);
 			}).fail(function(xhr, status, error) {
-                    deferred.reject(error);
-                  });
+          deferred.reject(error);
+          });
 
 			return deferred.promise;
 
-		}
+		},
+
+		searchFirebase: function(uid) {
+
+			var deferred = q.defer();
+			var query = $('#search').val();
+			var URL = 'https://movie-history-project.firebaseio.com/profiles'
+
+			$.ajax({
+				type: 'GET',
+				url: 'https://movie-history-project.firebaseio.com/profiles/' + uid +'.json'
+			}).done(function(firebaseData) {
+					deferred.resolve(firebaseData);
+					console.log('Search data from Firebase = ', firebaseData);
+			}).fail(function(xhr, status, error) {
+				deferred.reject(error);
+			});
+
+			return deferred.promise;
+
+		}		
 
 	}; // End of return statement
 
